d60f8f6f13790a9a0d805f776e046f9c7ca7d343
Christopherfrommaine <christopherfrommaine@gmail.com>
2025-02-18 19:05:37 -0500

    added footer, and main content to first page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,9 @@
 "use strict";
-// Replace global-header elements with global/header.html
+// Replace global-header elements with global/global-header.html
 document.addEventListener('DOMContentLoaded', function () {
     const headerElement = document.getElementById('global-header');
     if (headerElement) {
-        fetch('global/header.html')
+        fetch('global/global-header.html')
             .then(response => response.text())
             .then(data => {
             headerElement.innerHTML = data;
@@ -12,5 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     else {
         console.warn('Global header element not found on the page');
+    }
+});
+// Replace global-footer elements with global/global-footer.html
+document.addEventListener('DOMContentLoaded', function () {
+    const headerElement = document.getElementById('global-footer');
+    if (headerElement) {
+        fetch('global/global-footer.html')
+            .then(response => response.text())
+            .then(data => {
+            headerElement.innerHTML = data;
+        })
+            .catch(error => console.error('Error loading global footer:', error));
+    }
+    else {
+        console.warn('Global footer element not found on the page');
     }
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-// Replace global-header elements with global/header.html
+// Replace global-header elements with global/global-header.html
 document.addEventListener('DOMContentLoaded', function() {
     const headerElement = document.getElementById('global-header');
     
     if (headerElement) {
-      fetch('global/header.html')
+      fetch('global/global-header.html')
         .then(response => response.text())
         .then(data => {
           headerElement.innerHTML = data;
@@ -13,3 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
       console.warn('Global header element not found on the page');
     }
   });
+
+// Replace global-footer elements with global/global-footer.html
+document.addEventListener('DOMContentLoaded', function() {
+  const headerElement = document.getElementById('global-footer');
+  
+  if (headerElement) {
+    fetch('global/global-footer.html')
+      .then(response => response.text())
+      .then(data => {
+        headerElement.innerHTML = data;
+      })
+      .catch(error => console.error('Error loading global footer:', error));
+  } else {
+    console.warn('Global footer element not found on the page');
+  }
+});
